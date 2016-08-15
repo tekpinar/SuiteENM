b88a7cd303b1400087f52520417124d3e138474a
Mustafa Tekpinar <tekpinar@buffalo.edu>
2016-08-15 16:15:15 +0300

    Delete distance_functions.h

--- SuiteENM/include/distance_functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef DISTANCE_FUNCTIONS_H
-#define DISTANCE_FUNCTIONS_H
-
-double dis(CAcoord *atom, int i, int j);
-double disSquared(CAcoord *atom, int i, int j);
-double disAll(pdb *info, int i, int j);
-double disAll_pdb_v23(pdb_v23 *info, int i, int j);
-#endif
-
